4db8e533a53048aa0aad31ccf0ece858a25aa746
victimoftrap <monsquizo@gmail.com>
2020-11-30 15:22:41 +0700

    Updated project

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import math
 import random
-import stat_analysis
+import sample_characteristics
 
 
-def random_log(shift = 0, scale = 1):
+def random_log(shift=0, scale=1):
+    """Моделирование случайной величины логистического распределения.
+
+    :param shift: параматр сдвига, по-умолчанию 0
+    :param scale: параметр масштаба, по-умолчанию 1
+    :return: значение случайной величины.
+    """
     x = random.random()
     return math.log(x / (1 - x)) * scale + shift
 
 
-def to_isw_file(data, name, description, size):
+def save_to_isw_file(data, size, name, description):
+    """Записать датасет в файл формата ISW.
+
+    :param data: набор данных
+    :param size: размер набора данных
+    :param name: имя файла
+    :param description: описание записываемого набора данных
+    :return:
+    """
     with open(name, 'w') as file:
-        file.write(name + '\n')
+        file.write(description + '\n')
         file.write(f"0 {size}\n")
         for val in data:
             file.write(f"{val}\n")
 
 
 def generate_log(size, primary, secondary, contamination):
+    """Генерация набора данных.
+
+    :param size: количество значений в выборке
+    :param primary: функция чистого распределения
+    :param secondary: функция засорённого распределения
+    :param contamination: засорение
+    :return: набор данных
+    """
     res = []
     for i in range(size):
         r = random.random()
-        if (r <= 1 - contamination):
+        if r <= 1 - contamination:
             res.append(primary())
         else:
             res.append(secondary())
@@ -30,13 +52,18 @@ def generate_log(size, primary, secondary, contamination):
 def main():
     n = 100000
     data = generate_log(size=n, contamination=0.2, primary=lambda: random_log(), secondary=lambda: random_log(scale=10))
-    print(stat_analysis.mean(data))
-    print(stat_analysis.median(data))
-    print(stat_analysis.variance(data))
-    print(stat_analysis.skewness(data))
-    print(stat_analysis.kurtosis(data))
-    # to_isw_file(data, name='Log(0, 1)x0.8_mixt_scale10_100.dat', description='Логистическое(0,1) с засорением 0.2 логистическим (0,10)', size=n)
+    print(sample_characteristics.mean(data))
+    print(sample_characteristics.median(data))
+    print(sample_characteristics.variance(data))
+    print(sample_characteristics.skewness(data))
+    print(sample_characteristics.kurtosis(data))
+    save_to_isw_file(
+        data,
+        name='Log(0, 1)x0.8_mixt_scale10_100.dat',
+        description='Логистическое(0,1) с засорением 0.2 логистическим (0,10)',
+        size=n
+    )
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- sample_characteristics.py
+++ sample_characteristics.py
@@ -1,5 +1,4 @@
 import statistics
-import math
 
 
 def mean(data):
@@ -15,18 +14,28 @@ def variance(data):
 
 
 def skewness(data):
+    """Вычислить коэффициент асимметрии.
+
+    :param data: выборка
+    :return: значение коэффициента
+    """
     m = mean(data)
     s = 0
     for x in data:
-        s += pow((x-m), 3)
+        s += pow((x - m), 3)
     sigma_cube = pow(variance(data), 1.5)
     return s / (len(data) * sigma_cube)
 
 
 def kurtosis(data):
+    """Вычислить коэффициент эксцесса.
+
+    :param data: выборка
+    :return: значение коэффициента
+    """
     m = mean(data)
     s = 0
     for x in data:
-        s += pow((x-m), 4)
+        s += pow((x - m), 4)
     d_squared = pow(variance(data), 2)
     return s / (len(data) * d_squared)
