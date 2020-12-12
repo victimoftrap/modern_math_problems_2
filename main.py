b4f928159b38fa81c0fc959a7309340b95593ea6
Daniil Poliakov <d.poliakov@yahoo.com>
2020-12-13 01:52:35 +0700

    added estimations

--- main.py
+++ main.py
@@ -5,6 +5,38 @@ import argparse
 import sample_characteristics
 
 
+def secant_method(f, x0, epsilon, data, scale, delta=None):
+    f_0 = f(x0, data, scale, delta)
+    f_prime_0 = (f_0 - f(x0 - 0.1, data, scale, delta)) / 0.1
+    x_i1 = x0
+    x_i2 = x0 - f_0 / f_prime_0
+    f_i1 = f_0
+    f_i2 = f(x_i2, data, scale, delta)
+    while math.fabs(f_i2 - f_i1) > epsilon:
+        t = x_i2 - (f_i2 / (f_i2 - f_i1)) * (x_i2 - x_i1)  
+        x_i1 = x_i2
+        f_i1 = f_i2
+        x_i2 = t
+        f_i2 = f(t, data, scale, delta)   
+    return x_i2
+    
+    
+def estimator_mle_log(theta, data, scale, delta):
+    sum = 0
+    for y in data:
+        t = math.exp((theta - y) / scale)
+        sum += (t - 1) / (t + 1)
+    return sum
+    
+    
+def estimator_oro_log(theta, data, scale, delta):
+    sum = 0
+    for y in data:
+        t = math.exp((theta - y) / scale)
+        sum += math.pow(t, delta) / math.pow((t + 1), 2*delta+1) * (t - 1)
+    return sum
+    
+
 def random_log(shift=0, scale=1):
     """Моделирование случайной величины логистического распределения.
 
@@ -56,18 +88,24 @@ def main(n, shift, scale, contamination):
         size=n,
         contamination=contamination,
         primary=lambda: random_log(),
-        secondary=lambda: random_log(scale=scale)
+        secondary=lambda: random_log(shift=shift, scale=scale)
     )
-    print("Среднее:", sample_characteristics.mean(data))
+    mean = sample_characteristics.mean(data)
+    print("Среднее:", mean)
     print("Медиана:", sample_characteristics.median(data))
     print("Дисперсия:", sample_characteristics.variance(data))
     print("Коэф. асимметрии:", sample_characteristics.skewness(data))
     print("Коэф. эксцесса:", sample_characteristics.kurtosis(data))
+    print("Оценка максимального правдоподобия: ", secant_method(estimator_mle_log, mean, 0.0000001, data, scale))
+    print("Обобщенные радикальные оценки (delta=0.1): ", secant_method(estimator_oro_log, mean, 0.0000001, data, scale, 0.1))
+    print("Обобщенные радикальные оценки (delta=0.5): ", secant_method(estimator_oro_log, mean, 0.001, data, scale, 0.5))
+    #print("Обобщенные радикальные оценки (delta=1): ", secant_method(estimator_oro_log, mean, 0.0000001, data, scale, 1))
+    #print("Обобщенные радикальные оценки (delta=0.7): ", secant_method(estimator_oro_log, mean, 0.0000001, data, scale, 0.7))
 
     save_to_isw_file(
         data,
-        name=f'Log(0, 1)x{1 - contamination}_mixt_scale{scale}_{n}.dat',
-        description=f'Логистическое(0,1) с засорением {contamination} логистическим (0,{scale})',
+        name=f'Log(0, 1)x{1 - contamination}_mixt_shift{shift}_scale{scale}_{n}.dat',
+        description=f'Логистическое(0,1) с засорением {contamination} логистическим ({shift},{scale})',
         size=n
     )
 
